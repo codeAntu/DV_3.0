669e1b04e81aaf74e06e1e9deee3ab425f29631d
Ananta <codeantu@gmail.com>
2024-09-04 18:02:58 +0530

    chore: Update index.css and SplashScreen.tsx

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -2,9 +2,8 @@
 
 export default function SplashScreen() {
   return (
-    <div className='flexCenter h-[100dvh] w-full bg-accent  '>
-      {/* <img src='./icons/appLogoSS.svg' alt='' className='w-1/4' /> */}
-      <div className='text-3xl font-bold text-primary'>Loading...</div>
+    <div className='h-[100dvh] bg-accent flexCenter  '>
+      <img src='./icons/appIconWhite.svg' alt='' className='w-1/4' />
     </div>
   )
 }
