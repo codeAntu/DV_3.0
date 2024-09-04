eda424ccbb0b969a7f0f7ba91432a3f72b23d48f
Ananta <codeantu@gmail.com>
2024-09-04 17:52:50 +0530

    chore: Add SplashScreen component to App and main files

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import SplashScreen from './screens/SplashScreen.tsx'
+import "./css/index.css"
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    {/* <SplashScreen /> */}
   </StrictMode>,
 )
---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,10 @@
+
+
+export default function SplashScreen() {
+  return (
+    <div className='flexCenter h-[100dvh] w-full bg-accent  '>
+      {/* <img src='./icons/appLogoSS.svg' alt='' className='w-1/4' /> */}
+      <div className='text-3xl font-bold text-primary'>Loading...</div>
+    </div>
+  )
+}
