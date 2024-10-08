c4fa9afa5e6cc37ee193f222423dc6ec5e7a9602
Ananta <codeantu@gmail.com>
2024-10-09 00:36:48 +0530

    refactor: Update profile.html and sidebar.html
    - Update profile.html to fix a styling issue with the profile card.
    - Update sidebar.html to add overflow auto to the menu.

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./css/index.css"
 import Nav from './screens/Home.tsx'
+import Profile from './screens/Profile.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Nav />
+    <Profile />
   </StrictMode>,
 )
